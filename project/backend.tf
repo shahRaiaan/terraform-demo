af47fa3726d5ccea2acdf3d8aa26559ba9751bcb
Shah Raiaan <smraiaan@gmail.com>
2020-12-02 11:31:49 -0600

    Delete backend.tf file

--- project/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "raiaan-remotebackend-bucket1"
-    key            = "remotetfstate"
-    region         = "us-east-2"
-    dynamodb_table = "s3-state-lock"
-  }
-}
